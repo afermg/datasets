5e19ccdca615de1cf202b41f99ea867b6792e972
Alan Munoz <afer.mg@gmail.com>
2024-07-26 12:27:13 -0400

    change(flake): use standard devenv flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,56 @@
 # Flake to set up an environment to upload profile_index.csv to Zenodo.
 {
-  inputs.nix-github-actions.url = "github:nix-community/nix-github-actions";
-  inputs.nix-github-actions.inputs.nixpkgs.follows = "nixpkgs";
-
-  outputs = { self, nixpkgs, nix-github-actions }: {
-    githubActions = nix-github-actions.lib.mkGithubMatrix { inherit (self) checks; };
-    checks.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    checks.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs_master.url = "github:NixOS/nixpkgs/master";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
   };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = import nixpkgs {
+              system = system;
+              config.allowUnfree = true;
+            };
+
+            mpkgs = import inputs.nixpkgs_master {
+              system = system;
+              config.allowUnfree = true;
+            };
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  packages = with pkgs; [
+                    git
+                    curl
+                    jq
+                  ];
+                  enterShell = ''
+                  echo "Shell entered"
+                  '';
+                }
+              ];
+            };
+          });
+    };
 }
